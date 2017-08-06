74d3b30cefee711602abdca2fee86a11b998fec3
OHTA Shogo <athos0220@gmail.com>
2017-08-06 16:35:50 +0900

    Drop support for CLJ < v1.7 to make .cljc available

--- project.clj
+++ project.clj
@@ -4,12 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.6.1"
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/clojurescript "1.9.854"]
                  [org.clojure/math.combinatorics "0.1.3"]]
-  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+  :profiles {:1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
              :dev {:source-paths ["examples"]}}
-  :aliases {"all" ["with-profile" "dev:1.4:1.5:1.6:1.7"]})
+  :aliases {"all" ["with-profile" "dev:1.7:1.8"]})
