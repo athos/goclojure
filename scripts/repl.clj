c9cf0753d491c0a52ab710ced10499e4a000b91c
OHTA Shogo <athos0220@gmail.com>
2017-08-12 05:22:44 +0900

    Simplify REPL script

--- scripts/repl.clj
+++
@@ -1,8 +0,0 @@
-(require '[cljs.repl :as repl]
-         '[cljs.repl.node :as node])
-
-(repl/repl* (node/repl-env)
-  {:output-dir "target/out"
-   :optimizations :none
-   :cache-analysis true
-   :source-map true})
